fe42611bd52bbda7de59780b1dd96030d4a716f0
SureshJoel <sureshkariki@gmail.com>
2026-04-13 22:13:14 -0400

    Expoloring Routing concepts

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ name: "John", age: 30 });
+});
+
+app.post("/updateuser", (req, res) => {
+  res.send("User created successfully!");
+});
+
+// Basically, "use" matches all HTTP methods (GET, POST, etc.) and all paths.
+// So, it will match any request that comes to the server, regardless of the method or path.
+// In this case, it will respond with "Hello new test URL routing!!" for any request that starts with "/test".
 app.use("/test", (req, res) => {
   res.send("Hello new test URL routing!!");
 });
 
-// app.use("/base", (req, res) => {
-//   res.send("This is added as one more URL routing!!");
-// });
+app.use("/base", (req, res) => {
+  res.send("This is added as one more URL routing!!");
+});
 
 // // Request handler
 // app.use((req, res) => {
